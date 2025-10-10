631fd9af0d619fe3bb5f3a5ed92c84cf2e2a50ce
kolorrombeng <1811511250@student.budiluhur.ac.id>
2025-10-10 19:48:43 +0700

    Updated Particles2D.tsx

--- src/components/Particles2D.tsx
+++ src/components/Particles2D.tsx
@@ -6,7 +6,7 @@ import { useTheme } from 'next-themes';
 // --- KONFIGURASI INTERAKSI ---
 const PARTICLE_COUNT = 3000;     // Jumlah partikel (lebih sedikit untuk 2D agar tetap ringan)
 const INTERACTION_RADIUS = 100;   // Jarak radius interaksi dari kursor (dalam pixel)
-const REPULSION_STRENGTH = 3;    // Kekuatan partikel menjauh dari kursor
+const REPULSION_STRENGTH = 1,5;    // Kekuatan partikel menjauh dari kursor
 const RETURN_SPEED = 0.01;       // Kecepatan partikel kembali ke posisi semula
 const PARTICLE_SIZE = 0.2;         // Ukuran partikel
 
